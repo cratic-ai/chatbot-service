1908bce9815af7eb06236637ca2db3aa7cdf9962
cratic-ai <craticai08@gmail.com>
2025-12-15 16:30:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('trust proxy', 1);
 // ============================================
 const allowedOrigins = [
   'https://mfgcompliance-cai.vercel.app',
+   'https://works.craticai.com',
   'https://mfgcompliance.craticai.com',
   'http://localhost:5173',
   'http://localhost:3000',
